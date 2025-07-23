428fb8c2212f3c8aa058106fdf4fc688240e4f90
Sheetal-jagotra <sheetal95jagotra@gmail.com>
2025-07-23 18:56:51 +0530

    APITesting020_TestNG_Enabled_TestNG

---
+++ MyProjects/ATB10XAPIAutomationPrograms/src/test/java/ex04_POST_Request/APITesting020_TestNG_Enabled.java
@@ -0,0 +1,17 @@
+package ex04_POST_Request;
+
+import jdk.jfr.Enabled;
+import org.testng.Assert;
+
+public class APITesting020_TestNG_Enabled {
+
+    public void test1(){
+        Assert.assertTrue(true);
+    }
+    public void test2(){
+        Assert.assertTrue(true);
+    }
+    public void test3(){
+        Assert.assertTrue(true);
+    }
+}
